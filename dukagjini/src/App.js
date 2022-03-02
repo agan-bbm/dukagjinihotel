7b3efd5a5e6d454d88cde75003f0b7ae38554330
agan-bbm <agan@blackbird.marketing>
2022-03-02 11:15:58 +0100

    First Commit

---
+++ dukagjini/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Homepage from "./components/Pages/Homepage";
+
+console.log(
+  "%c Developed by: Black Bird Marketing https://blackbird.marketing",
+  "padding:9px 5px; background:black; line-height:25px;color: white"
+);
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ dukagjini/src/components/Pages/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <h2>Dukagjini</h2>
+    </div>
+  );
+}
+export default Homepage;
