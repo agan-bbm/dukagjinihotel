0b37766bb0fa9126c129688d97587770dce7fff3
agan-bbm <agan@blackbird.marketing>
2022-03-16 16:23:16 +0100

    u kry orik

--- dukagjini/src/components/Pages/Booking/BookingSection.js
+++ dukagjini/src/components/Pages/Booking/BookingSection.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 // CSS
 import "../Booking/booking.css";
-import { useSearchParams } from "react-router-dom";
 
 function BookingSection() {
   const [dates, setDates] = useState({
@@ -14,34 +13,23 @@ function BookingSection() {
     rooms: [],
     isLoaded: false,
   });
-  const [searchParams, setSearchParams] = useState(null);
-  // async function getRooms() {
-  //   // console.log(
-  //   //   `http://cmsdukagjini.blackbird.marketing/wp-content/fetch.php?fromDate=${dates.from}&toDate=${dates.to}`
-  //   // );
 
-  //   await axios
-  //     .get(
-  //       `http://cmsdukagjini.blackbird.marketing/wp-content/fetch.php?fromDate=${dates.from}&toDate=${dates.to}`,
-  //       {
-  //         headers: {
-  //           "Content-Type": "text/plain",
-  //         },
-  //       }
-  //     )
-  //     .then((res) =>
-  //       setRooms({
-  //         rooms: JSON.parse(res.data),
-  //         isLoaded: true,
-  //       })
-  //     )
-  //     .catch((err) => console.log(err));
-  // }
-
-  console.log(
-    new URLSearchParams(window.location.search).get("rooms"),
-    "====="
-  );
+  async function getRooms() {
+    await axios
+      .get(
+        `http://cmsdukagjini.blackbird.marketing/wp-content/fetch.php?fromDate=${dates.from}&toDate=${dates.to}`,
+        {}
+      )
+      .then((res) => {
+        setRooms({
+          ...rooms,
+          rooms: res.data,
+          isLoaded: true,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  console.log(rooms.rooms);
 
   const formatDate = (date) => {
     let d = new Date(date);
@@ -56,56 +44,42 @@ function BookingSection() {
     }
     return [year, month, day].join("/");
   };
-  // console.log("from", dates.from);
-  // console.log("to", dates.to);
   var today = new Date().toISOString().split("T")[0];
-  // console.log(rooms.rooms);
   return (
     <div className="booking-main">
-      <form
-        method="GET"
-        action="http://cmsdukagjini.blackbird.marketing/wp-content/fetch.php"
-      >
-        <div className="inputs-container">
-          <label htmlFor="start">Start Date</label>
-          <input
-            type="date"
-            id="start"
-            name="fromDate"
-            required
-            min={today}
-            onChange={(e) => {
-              setDates({ ...dates, from: formatDate(e.target.value) });
-            }}
-          />
-        </div>
-        <div className="inputs-container">
-          <label htmlFor="end">End Date</label>
-          <input
-            type="date"
-            id="end"
-            name="toDate"
-            min={today}
-            required
-            onChange={(e) => {
-              setDates({ ...dates, to: formatDate(e.target.value) });
-            }}
-          />
-        </div>
-        {/* <iframe
-          name="testIframe"
-          style={{ display: "none", width: "0", height: "0" }}
-        ></iframe> */}
-        <button
-          // type="submit"
-          formTarget="testIframe"
-          onClick={() => {
-            // getRooms();
+      <div className="inputs-container">
+        <label htmlFor="start">Start Date</label>
+        <input
+          type="date"
+          id="start"
+          name="fromDate"
+          required
+          min={today}
+          onChange={(e) => {
+            setDates({ ...dates, from: formatDate(e.target.value) });
           }}
-        >
-          Book
-        </button>
-      </form>
+        />
+      </div>
+      <div className="inputs-container">
+        <label htmlFor="end">End Date</label>
+        <input
+          type="date"
+          id="end"
+          name="toDate"
+          min={today}
+          required
+          onChange={(e) => {
+            setDates({ ...dates, to: formatDate(e.target.value) });
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          getRooms();
+        }}
+      >
+        Book
+      </button>
     </div>
   );
 }
