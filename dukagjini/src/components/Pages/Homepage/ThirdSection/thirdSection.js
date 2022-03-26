0299eeab077fad13b3c458aa2573ad7d18a7aadf
rgnpcrz <48785434+rgnpcrz@users.noreply.github.com>
2022-03-26 01:46:36 +0100

    swiperSlider

---
+++ dukagjini/src/components/Pages/Homepage/ThirdSection/thirdSection.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+//CSS
+import "./thirdSection.css";
+
+// SLIDER
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+//import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+import Img1 from "../../../../Assets/hotel/1.jpg";
+import Img2 from "../../../../Assets/hotel/2.jpg";
+import Img3 from "../../../../Assets/hotel/3.jpg";
+import Img4 from "../../../../Assets/hotel/4.jpg";
+
+function ThirdSection() {
+  return (
+    <div className="thirdSection">
+        <div className="containerWrapper" >
+            <div className="thirdSection1">
+                <Swiper
+                    slidesPerView={2}
+                    centeredSlides={true}
+                    spaceBetween={30}
+                    grabCursor={true}
+                    navigation
+                    // pagination={{
+                    //   clickable: true,
+                    // }}
+                    modules={[Navigation]}
+                    className="mySwiper"
+                    loop={true}
+                    >
+
+                    <SwiperSlide>
+                        <img src={Img1} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img2} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img3} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img4} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img2} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img1} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <img src={Img4} />
+                        </SwiperSlide>
+                    </Swiper>
+    
+            <div className="thirdSectionBottom">
+                <div className="thirdSectionLeft"><h2 className="thirdSectionHeader">The shelter is the Outcome of the Dream</h2></div>
+                    <div className="thirdSectionRight">
+                        <p className='thirdSectionParagraph'>Broker ipsum dolor sit amet, consectetuer luxury elit. Carrara marble commodo ligula eget dolor cum sociis cozy penatibus et magnis.</p>
+                        <p className='thirdSectionParagraph'>Ochean de View extends along a private, quiet and beautiful tropical beach. Stay away from the crowd and enjoy the beauty and lust of Cancun with us.</p>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default ThirdSection
