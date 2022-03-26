b7795ee28f948c95e0f6c80ff205b52b8a81fbf8
rgnpcrz <48785434+rgnpcrz@users.noreply.github.com>
2022-03-26 01:57:08 +0100

    img src fix

--- dukagjini/src/Pages/Homepage.js
+++ dukagjini/src/Pages/Homepage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import FirstSection from "../components/Pages/Homepage/FirstSection/FirstSection";
 import SecondSection from "../components/Pages/Homepage/SecondSection/SecondSection";
+import ThirdSection from "../components/Pages/Homepage/ThirdSection/thirdSection";
 
 function Homepage() {
   return (
     <>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </>
   );
 }
