16bcb9818d04c564586735261e4f6839285d4d76
agan-bbm <agan@blackbird.marketing>
2022-04-14 14:22:07 +0200

    new commit

--- dukagjini/src/components/Pages/Rooms/index.js
+++ dukagjini/src/components/Pages/Rooms/index.js
@@ -16,10 +16,10 @@ import penthouse from "../../../images/penthouse.jpg";
 
 //icons
 
-import livingroom from "../../../images/livingroom.png";
-import bath from "../../../images/bath.png";
-import seaview from "../../../images/seaview.png";
-import balcony from "../../../images/balcony.png";
+import livingroom from "../../../images/user.svg";
+import bath from "../../../images/bed.svg";
+import seaview from "../../../images/dashboard.svg";
+import balcony from "../../../images/wifi.svg";
 
 export function Rooms() {
   return (
@@ -59,9 +59,9 @@ export function Rooms() {
                       A 120sq foot bedroom apartment with a view.
                     </p>
                   </div>
-                  <div className="price-night">
+                  <div className="price-night-desktop">
                     <h4 id="price">$120,00</h4>
-                    <p id="nights">1 night/ 2 persons</p>
+                    <p id="nights">1 night, 2 persons</p>
                   </div>
                 </div>
                 <div className="icon-info">
@@ -84,73 +84,268 @@ export function Rooms() {
                     <p>Living Room</p>
                   </div>
                 </div>
-                “Elegant, inviting and with a wonderful view over the city and
-                the beautiful park – this is how my hotelier imagined his
-                Sun-Room and I have to admit that I am proud of what he made of
-                me.”
+                <p style={{ color: "#575757" }}>
+                  “Elegant, inviting and with a wonderful view over the city and
+                  the beautiful park – this is how my hotelier imagined his
+                  Sun-Room and I have to admit that I am proud of what he made
+                  of me.”
+                </p>
+
                 <div className="room-buttons">
+                  <div className="price-night-mobile">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
                   <div className="book-button">
-                    <button>BOOK</button>
+                    <button>Reserve</button>
                   </div>
-                  <div className="details-button">
+                  {/* <div className="details-button">
                     <a href="">View</a>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
           </div>
 
           <div className="single-room">
-            <h2 className="room-title">Standard Double Queen Room</h2>
-            <div>
-              <Swiper
-                modules={[Navigation]}
-                spaceBetween={50}
-                slidesPerView={1}
-                navigation
-                // onSlideChange={() => console.log("slide change")}
-                // onSwiper={(swiper) => console.log(swiper)}
-              >
-                <SwiperSlide>
-                  <img src={standard} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={deluxequeen} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={deluxetwin} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={penthouse} alt="" />
-                </SwiperSlide>
-              </Swiper>
-            </div>
-            <div className="icon-info">
-              <div className="icon">
-                <img src={seaview} alt="" />
-                <p>Sea view</p>
+            <div className="singleroom-flex">
+              <div className="room-img-gallery">
+                <Swiper
+                  modules={[Navigation]}
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  navigation
+                  // onSlideChange={() => console.log("slide change")}
+                  // onSwiper={(swiper) => console.log(swiper)}
+                >
+                  <SwiperSlide>
+                    <img src={standard} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxequeen} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxetwin} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={penthouse} alt="" />
+                  </SwiperSlide>
+                </Swiper>
               </div>
-              <div className="icon">
-                <img src={balcony} alt="" />
-                <p>Balcony </p>
-              </div>
+              <div className="room-desc-info">
+                <div className="main-room-infos">
+                  <div className="room-name-shortdesc">
+                    <h2 className="room-title">Standard Double Queen Room</h2>
+                    <p className="room-short-desc">
+                      A 120sq foot bedroom apartment with a view.
+                    </p>
+                  </div>
+                  <div className="price-night-desktop">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                </div>
+                <div className="icon-info">
+                  <div className="icon-rooms-page">
+                    <img src={seaview} alt="" />
+                    <p>Sea view</p>
+                  </div>
+                  <div className="icon-rooms-page">
+                    <img src={balcony} alt="" />
+                    <p>Balcony </p>
+                  </div>
 
-              <div className="icon">
-                <img src={bath} alt="" />
-                <p>Bath</p>
-              </div>
+                  <div className="icon-rooms-page">
+                    <img src={bath} alt="" />
+                    <p>Bath</p>
+                  </div>
 
-              <div className="icon">
-                <img src={livingroom} alt="" />
-                <p>Living Room</p>
+                  <div className="icon-rooms-page">
+                    <img src={livingroom} alt="" />
+                    <p>Living Room</p>
+                  </div>
+                </div>
+                <p style={{ color: "#575757" }}>
+                  “Elegant, inviting and with a wonderful view over the city and
+                  the beautiful park – this is how my hotelier imagined his
+                  Sun-Room and I have to admit that I am proud of what he made
+                  of me.”
+                </p>
+
+                <div className="room-buttons">
+                  <div className="price-night-mobile">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                  <div className="book-button">
+                    <button>Reserve</button>
+                  </div>
+                  {/* <div className="details-button">
+                    <a href="">View</a>
+                  </div> */}
+                </div>
               </div>
             </div>
-            <div className="room-buttons">
-              <div className="book-button">
-                <button>BOOK</button>
+          </div>
+
+          <div className="single-room">
+            <div className="singleroom-flex">
+              <div className="room-img-gallery">
+                <Swiper
+                  modules={[Navigation]}
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  navigation
+                  // onSlideChange={() => console.log("slide change")}
+                  // onSwiper={(swiper) => console.log(swiper)}
+                >
+                  <SwiperSlide>
+                    <img src={standard} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxequeen} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxetwin} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={penthouse} alt="" />
+                  </SwiperSlide>
+                </Swiper>
               </div>
-              <div className="details-button">
-                <a href="">View</a>
+              <div className="room-desc-info">
+                <div className="main-room-infos">
+                  <div className="room-name-shortdesc">
+                    <h2 className="room-title">Standard Double Queen Room</h2>
+                    <p className="room-short-desc">
+                      A 120sq foot bedroom apartment with a view.
+                    </p>
+                  </div>
+                  <div className="price-night-desktop">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                </div>
+                <div className="icon-info">
+                  <div className="icon-rooms-page">
+                    <img src={seaview} alt="" />
+                    <p>Sea view</p>
+                  </div>
+                  <div className="icon-rooms-page">
+                    <img src={balcony} alt="" />
+                    <p>Balcony </p>
+                  </div>
+
+                  <div className="icon-rooms-page">
+                    <img src={bath} alt="" />
+                    <p>Bath</p>
+                  </div>
+
+                  <div className="icon-rooms-page">
+                    <img src={livingroom} alt="" />
+                    <p>Living Room</p>
+                  </div>
+                </div>
+                <p style={{ color: "#575757" }}>
+                  “Elegant, inviting and with a wonderful view over the city and
+                  the beautiful park – this is how my hotelier imagined his
+                  Sun-Room and I have to admit that I am proud of what he made
+                  of me.”
+                </p>
+
+                <div className="room-buttons">
+                  <div className="price-night-mobile">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                  <div className="book-button">
+                    <button>Reserve</button>
+                  </div>
+                  {/* <div className="details-button">
+                    <a href="">View</a>
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="single-room">
+            <div className="singleroom-flex">
+              <div className="room-img-gallery">
+                <Swiper
+                  modules={[Navigation]}
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  navigation
+                  // onSlideChange={() => console.log("slide change")}
+                  // onSwiper={(swiper) => console.log(swiper)}
+                >
+                  <SwiperSlide>
+                    <img src={standard} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxequeen} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={deluxetwin} alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={penthouse} alt="" />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+              <div className="room-desc-info">
+                <div className="main-room-infos">
+                  <div className="room-name-shortdesc">
+                    <h2 className="room-title">Standard Double Queen Room</h2>
+                    <p className="room-short-desc">
+                      A 120sq foot bedroom apartment with a view.
+                    </p>
+                  </div>
+                  <div className="price-night-desktop">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                </div>
+                <div className="icon-info">
+                  <div className="icon-rooms-page">
+                    <img src={seaview} alt="" />
+                    <p>Sea view</p>
+                  </div>
+                  <div className="icon-rooms-page">
+                    <img src={balcony} alt="" />
+                    <p>Balcony </p>
+                  </div>
+
+                  <div className="icon-rooms-page">
+                    <img src={bath} alt="" />
+                    <p>Bath</p>
+                  </div>
+
+                  <div className="icon-rooms-page">
+                    <img src={livingroom} alt="" />
+                    <p>Living Room</p>
+                  </div>
+                </div>
+                <p style={{ color: "#575757" }}>
+                  “Elegant, inviting and with a wonderful view over the city and
+                  the beautiful park – this is how my hotelier imagined his
+                  Sun-Room and I have to admit that I am proud of what he made
+                  of me.”
+                </p>
+
+                <div className="room-buttons">
+                  <div className="price-night-mobile">
+                    <h4 id="price">$120,00</h4>
+                    <p id="nights">1 night, 2 persons</p>
+                  </div>
+                  <div className="book-button">
+                    <button>Reserve</button>
+                  </div>
+                  {/* <div className="details-button">
+                    <a href="">View</a>
+                  </div> */}
+                </div>
               </div>
             </div>
           </div>
