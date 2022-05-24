3e1d6cdf336f95375379e4054601ab6f017a1cc7
agan-bbm <agan@blackbird.marketing>
2022-05-24 14:58:44 +0200

    6

--- dukagjini/src/components/Pages/Thankyou/Thankyou.js
+++ dukagjini/src/components/Pages/Thankyou/Thankyou.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Thankyou.css";
+import tick from "../../../images/Tick.svg";
 function Thankyou({ posts }) {
   console.log(posts);
   return (
     <>
       <div className="tu-container">
+        <img src={tick} alt="" />
         <h2 className="tu-heading">{posts.heading}</h2>
         <p className="tu-paragraph">{posts.paragraph}</p>
         <p className="tu-contact">
-          {posts.phone} <br /> {posts.email}
+          <span>{posts.phone}</span> | <span>{posts.email}</span>
         </p>
 
-        <Link to="/">
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
           <p className="tu-back">{posts.backtohomepage}</p>
         </Link>
       </div>
