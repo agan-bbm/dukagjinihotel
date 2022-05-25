5ba033c45ef222ad74165b0a3aae0daaadbc469a
Orik Gashi <82452760+gashiorik@users.noreply.github.com>
2022-05-25 11:52:30 +0200

    error page

--- dukagjini/src/components/Pages/ReservationError.js
+++ dukagjini/src/components/Pages/ReservationError.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/index";
+import { Link } from "react-router-dom";
 
 function ReservationError() {
   return (
     <>
-      <div style={{ padding: "150px" }}>errorrrrrr</div>
+      <div className="error-page">
+        <h1>Please try again</h1>
+        <h2>Something went wrong</h2>
+        <Link to="/booking">BOOK NOW</Link>
+        <Link to="/update-reservation">Update Reservation</Link>
+        <Link to="/delete-reservation">Delete Reservation</Link>
+      </div>
     </>
   );
 }
